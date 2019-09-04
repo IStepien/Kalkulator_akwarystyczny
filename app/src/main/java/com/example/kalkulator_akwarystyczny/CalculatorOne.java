873fc00be95b2f04e3aa050b110764f81c8e53db
Irmina Stepien <irm.stepien@gmail.com>
2019-09-04 11:05:28 +0200

    New layout for calculatorOne added; Fragment for additional info added

--- app/src/main/java/com/example/kalkulator_akwarystyczny/CalculatorOne.java
+++ app/src/main/java/com/example/kalkulator_akwarystyczny/CalculatorOne.java
@@ -5,23 +5,23 @@ import lombok.Data;
 @Data
 public class CalculatorOne {
 
-    private Double aquariumCapacity;
-    private Double dailyDose;
-    private Double fertilizerQuantity;
-    private Double Kquantity;
-    private Double NOquantity;
-    private Double POquantity;
-    private Double MGquantity;
+//    private Double aquariumCapacity;
+//    private Double dailyDose;
+//    private Double fertilizerQuantity;
+//    private Double Kquantity;
+//    private Double NOquantity;
+//    private Double POquantity;
+//    private Double MGquantity;
+//
+//    public CalculatorOne(Double aquariumCapacity, Double dailyDose, Double fertilizerQuantity, Double Kquantity, Double NOquantity, Double POquantity, Double MGquantity) {
+//        this.aquariumCapacity = aquariumCapacity;
+//        this.dailyDose = dailyDose;
+//        this.fertilizerQuantity = fertilizerQuantity;
+//        this.Kquantity = Kquantity;
+//        this.NOquantity = NOquantity;
+//        this.POquantity = POquantity;
+//        this.MGquantity = MGquantity;
 
-    public CalculatorOne(Double aquariumCapacity, Double dailyDose, Double fertilizerQuantity, Double Kquantity, Double NOquantity, Double POquantity, Double MGquantity) {
-        this.aquariumCapacity = aquariumCapacity;
-        this.dailyDose = dailyDose;
-        this.fertilizerQuantity = fertilizerQuantity;
-        this.Kquantity = Kquantity;
-        this.NOquantity = NOquantity;
-        this.POquantity = POquantity;
-        this.MGquantity = MGquantity;
-    }
 
 //    public Double calculateK2SO4() {
 //        return calculateKgForK() / calculateKation(ElementsForCalcOne.K);
@@ -40,9 +40,9 @@ public class CalculatorOne {
 //        return counted(POquantity, ElementsForCalcOne.PO4);
 //    }
 
-    public Double calculateMgSO4x7H2O() {
-        return calculateCapacity(MGquantity) / calculateKation(ElementsForCalcOne.MG) * fertilizerQuantity / 1000;
-    }
+//    public Double calculateMgSO4x7H2O() {
+//        return calculateCapacity(MGquantity) / calculateKation(ElementsForCalcOne.MG) * fertilizerQuantity / 1000;
+//    }
 
 //    private Double calculateKgForK() {
 //        return (calculateCapacity(Kquantity) * fertilizerQuantity / 1000) - calculateKg(NOquantity, ElementsForCalcOne.NO3)
@@ -53,36 +53,36 @@ public class CalculatorOne {
 //        return counted(elementQuantity, elementsForCalcOne) * calculateKation(elementsForCalcOne);
 //    }
 
-    private Double calculateCapacity(Double elementQuantity) {
-        return elementQuantity * aquariumCapacity / (7 * dailyDose);
-    }
+//    private Double calculateCapacity(Double elementQuantity) {
+//        return elementQuantity * aquariumCapacity / (7 * dailyDose);
+//    }
 
 //    private Double calculateAnion(ElementsForCalcOne elementsForCalcOne) {
 //        return elementsForCalcOne.anion / elementsForCalcOne.molarMass;
 //
 //    }
 
-    private Double calculateKation(ElementsForCalcOne elementsForCalcOne) {
-        Double molarMass = elementsForCalcOne.molarMass;
-        Double kation=0.0;
-        switch (elementsForCalcOne){
-            case K:
-                molarMass = elementsForCalcOne.molarMass * 4.46;
-                kation=78.2;
-                break;
-            case KCl:
-                molarMass=74.55;
-                kation=39.1;
-                break;
-            case NO3:
-                molarMass=101.1;
-                kation=39.1;
-                break;
-        }
-
-
-        return kation / molarMass;
-    }
+//    private Double calculateKation(ElementsForCalcOne elementsForCalcOne) {
+//        Double molarMass = elementsForCalcOne.molarMass;
+//        Double kation=0.0;
+//        switch (elementsForCalcOne){
+//            case K:
+//                molarMass = elementsForCalcOne.molarMass * 4.46;
+//                kation=78.2;
+//                break;
+//            case KCl:
+//                molarMass=74.55;
+//                kation=39.1;
+//                break;
+//            case NO3:
+//                molarMass=101.1;
+//                kation=39.1;
+//                break;
+//        }
+//
+//
+//        return kation / molarMass;
+//    }
 
 //    private Double counted(Double elementQuantity, ElementsForCalcOne elementsForCalcOne) {
 //        return calculateCapacity(elementQuantity) / calculateAnion(elementsForCalcOne) * fertilizerQuantity / 1000;
--- app/src/main/java/com/example/kalkulator_akwarystyczny/CalculatorOneDetailActivity.java
+++ app/src/main/java/com/example/kalkulator_akwarystyczny/CalculatorOneDetailActivity.java
@@ -1,57 +1,33 @@
 package com.example.kalkulator_akwarystyczny;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class CalculatorOneDetailActivity extends AppCompatActivity {
+    @BindView(R.id.button_additionalInfo) Button button_additonalInfo;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator1_detail);
+        ButterKnife.bind(this);
 
-        final Double aquariumCapacity = 100.0;
-        final Double dailyDose = 10.0;
-        final Double fertilizerQuantity = 200.0;
-
-        final EditText editText_Kquantity = findViewById(R.id.editText_Kquantity);
-        final EditText editText_NOquantity = findViewById(R.id.editText_NOquantity);
-        final EditText editText_POquantity = findViewById(R.id.editText_POquantity);
-        final EditText editText_MGquantity = findViewById(R.id.editText_MGquantity);
-
-        Button btnCalculate = findViewById(R.id.button);
-        btnCalculate.setOnClickListener(new View.OnClickListener() {
+        button_additonalInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Double Kquantity = Double.valueOf(editText_Kquantity.getText().toString());
-                Double NOquantity = Double.valueOf(editText_NOquantity.getText().toString());
-                Double POquantity = Double.valueOf(editText_POquantity.getText().toString());
-                Double MGquantity = Double.valueOf(editText_MGquantity.getText().toString());
-
-                CalculatorOne calculatorOne = new CalculatorOne(aquariumCapacity, dailyDose, fertilizerQuantity, Kquantity, NOquantity, POquantity, MGquantity);
-
-                TextView K2SO4 = findViewById(R.id.textView_K2SO4);
-                TextView KCl = findViewById(R.id.textView_KCl);
-                TextView KH2PO4 = findViewById(R.id.textView_KH2PO4);
-                TextView KNO3 = findViewById(R.id.textView_KNO3);
-                TextView MgSO4x7H2O = findViewById(R.id.textView_MgSO4x7H2O);
-                TextView calcDescription = findViewById(R.id.textView_calcOne_description);
-                calcDescription.setText(R.string.calcOne_description);
-//
-//                K2SO4.setText(calculatorOne.calculateK2SO4().toString());
-//                KCl.setText(calculatorOne.calculateKCl().toString());
-//                KH2PO4.setText(calculatorOne.calculateKH2PO4().toString());
-//                KNO3.setText(calculatorOne.calculateKNO3().toString());
-//                MgSO4x7H2O.setText(calculatorOne.calculateMgSO4x7H2O().toString());
+                Intent intent = new Intent(getApplicationContext(), AdditionalInfoActivity.class);
+                startActivity(intent);
             }
         });
-
 
     }
 
